e231dc4f4ac6be6a301040a4f4b60dbed5df24e6
Pom <kasiphit@gmail.com>
2025-09-16 17:04:51 +0700

    fix: fix error text in page by deleted unused api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,10 @@
 "use client";
 
 import Hero from "../components/Hero";
-import { useQuery } from "convex/react";
-import { api } from "convex/_generated/api";
-
 
 export default function Home() {
-  const bookings = useQuery(api.bookings.list);
   return (
     <main>
-      {bookings?.map(({ _id, name }) => (
-        <div key={_id}>{name}</div>
-      ))}
       <Hero />
     </main>
   );
